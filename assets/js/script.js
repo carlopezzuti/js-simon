cbea2a8f965ef1453112e1cef1205a15a5e1ad6a
carlopezzuti <carlopezzuti89@gmail.com>
2024-03-20 10:00:22 +0100

    push correct file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,47 @@
-const buttonPlay = document.querySelector("#btn-play");
-const numbersDiv = document.querySelector("#numbers");
-const result = document.querySelector("#result");
-const userNumbers = [];
-let correctNumbers = [];
+const container = document.querySelector(".container");
+const btn = document.querySelector("button")
 
-buttonPlay.addEventListener("click", function(){ 
-    result.innerHTML = ""; 
-    let numbersArray = generateCasualNumbers();
-    numbersDiv.innerHTML = numbersArray;
-    let correctNumbers = []; 
-    setTimeout(() => numbersDiv.innerHTML = "", 30000); 
-    
-    setTimeout(() => {
-        for (let i = 0; i < 5; i++) {
-            const inputNumber = parseInt(prompt("Inserisci un numero da 1 a 100:"));
-            userNumbers.push(inputNumber);
+btn.addEventListener("click", function(){
+    const simonArray = generateRandomNumbers(5, 20);
+    container.innerHTML = simonArray.join(" ");
 
-            if (numbersArray.includes(inputNumber)) {
-                correctNumbers.push(inputNumber);
-            }
+    setTimeout(() => container.innerHTML = "", 3000)
+
+    setTimeout(function(){
+        let userArray = [];
+        for(let i = 1; i <= 5; i++){
+            let userNumber = parseInt(prompt(`Scrivi il ${i}° numero che ricordi`));
+            userArray.push(userNumber)  
         }
-        result.innerHTML = `Hai indovinato ${correctNumbers.length} numeri: ${correctNumbers}`;     
-        
-    }, 31000); 
-});
+        let comparation = whichBelongsTo(simonArray, userArray);
 
-function generateCasualNumbers() {   
-    let uniqueNumbers = [];
-    while (uniqueNumbers.length < 5) {
-        let casualNumbers = Math.floor(Math.random() * 100) + 1;
-        if (!uniqueNumbers.includes(casualNumbers)) {
-            uniqueNumbers.push(casualNumbers);
+        if (comparation.length === 5){
+            alert(`Hai vinto! I numeri erano ${simonArray.join(" ")} e tu li hai indovinati tutti!`);
+        } else {
+            alert(`Hai perso. I numeri erano ${simonArray.join(" ")} e tu hai invece scritto ${userArray.join(" ")}, per un totale di ${comparation.length} punti.`)
+        }
+    }, 3500)
+})
+
+function generateRandomNumbers(num, max){
+    let randomNumbers = []
+    while(randomNumbers.length < num){
+        let newNum = Math.floor(Math.random() * (max) +1);
+        if (!randomNumbers.includes(newNum)){
+            randomNumbers.push(newNum);
         }
     }
-    return uniqueNumbers;
+    return randomNumbers;
+}
+
+function whichBelongsTo(array1, array2){
+    let numbers = []
+    for (let i = 0; i < 5; i++){
+        console.log(array2[i])
+        if (array1.includes(array2[i])){
+            console.log("Appartiene", array2[i])
+            numbers.push(array2[i])
+        }
+    }
+    return numbers
 }
